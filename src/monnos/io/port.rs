6381bd4fb0c7c6f99d0b364dcdeac6943402b1f6
Max Kofler <kofler.max.dev@gmail.com>
2024-06-08 09:22:13 +0200

    io: Add bit output functions

--- src/monnos/io/port.rs
+++ src/monnos/io/port.rs
@@ -11,6 +11,16 @@ pub fn out8(port: u16, data: u8) -> Result<(), PortIOError> {
     Ok(asm::out8(port, data))
 }
 
+/// Changes a single bit in at `port` by reading it and setting `bit` at `pos`
+/// # Arguments
+/// * `port` - The port to act on
+/// * `pos` - The position of the bit to manipulate
+/// * `bit` - The bit to set or unset
+pub fn out8_bit(port: u16, pos: u8, bit: bool) -> Result<(), PortIOError> {
+    let byte = in8(port)? | 1 << pos;
+    out8(port, byte & ((bit as u8) << pos))
+}
+
 /// Output two bytes to the IO port of the CPU
 /// * `port` - The port to output to
 /// * `data` - The data to output
@@ -18,11 +28,31 @@ pub fn out16(port: u16, data: u16) -> Result<(), PortIOError> {
     Ok(asm::out16(port, data))
 }
 
+/// Changes a single bit in at `port` by reading it and setting `bit` at `pos`
+/// # Arguments
+/// * `port` - The port to act on
+/// * `pos` - The position of the bit to manipulate
+/// * `bit` - The bit to set or unset
+pub fn out16_bit(port: u16, pos: u8, bit: bool) -> Result<(), PortIOError> {
+    let byte = in16(port)? | 1 << pos;
+    out16(port, byte & ((bit as u16) << pos))
+}
+
 /// Output four bytes to the IO port of the CPU
 /// * `port` - The port to output to
 /// * `data` - The data to output
 pub fn out32(port: u16, data: u32) -> Result<(), PortIOError> {
     Ok(asm::out32(port, data))
+}
+
+/// Changes a single bit in at `port` by reading it and setting `bit` at `pos`
+/// # Arguments
+/// * `port` - The port to act on
+/// * `pos` - The position of the bit to manipulate
+/// * `bit` - The bit to set or unset
+pub fn out32_bit(port: u16, pos: u8, bit: bool) -> Result<(), PortIOError> {
+    let byte = in32(port)? | 1 << pos;
+    out32(port, byte & ((bit as u32) << pos))
 }
 
 /// Receive one byte from the IO port of the CPU
